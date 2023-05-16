006a9ba368720f1186e340072a446c459c98d814
david <daviidgt03@gmail.com>
2023-05-16 20:16:01 +0200

    Add 5 cards

---
+++ src/types/type.ts
@@ -0,0 +1,6 @@
+import { Advisor } from '../components/advisor';
+import { Fighter } from '../components/fighter';
+import { King } from '../components/king';
+import { Squire } from '../components/squire';
+
+export type allTypes = King | Squire | Fighter | Advisor;
